2c429bb074267f37b7349e7f46073480af7d6fcf
drc <drccsadrs@gmail.com>
2024-11-19 17:40:30 +0800

    refactor: add binance_api_key and secret getter

--- src/config.py
+++ src/config.py
@@ -6,9 +6,17 @@ class Config:
     def __init__(self):
         load_dotenv()  # Load environment variables from .env file
         self.coingecko_api_key = os.getenv("COINGECKO_API_KEY")
+        self.binance_api_key = os.getenv("BINANCE_API_KEY")
+        self.binance_secret_key = os.getenv("BINANCE_SECRET_KEY")
 
     def get_coingecko_api_key(self):
         return self.coingecko_api_key
+    
+    def get_binanace_api_key(self):
+        return self.get_binanace_api_key
+    
+    def get_binace_secret_key(self):
+        return self.binance_secret_key
 
 # Create a single instance of Config
 config_instance = Config()
