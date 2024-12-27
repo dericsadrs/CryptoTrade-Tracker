5cf5979ace8f809ad97460587662c91b314dda23
drc <drccsadrs@gmail.com>
2024-12-27 18:20:58 +0800

    chore: add docstrirngs in methods of the BinanceClient class

--- src/services/binance/binance_client.py
+++ src/services/binance/binance_client.py
@@ -9,13 +9,29 @@ from config import config_instance
 logger = logging.getLogger(__name__)
 
 class BinanceClient:
+    """
+    A client for interacting with the Binance API.
+    """
+
     def __init__(self):
+        """
+        Initializes the BinanceClient with API keys from the configuration.
+        """
         self.client = Client(
             config_instance.get_binance_api_key(),
             config_instance.get_binance_secret_key()
         )
 
     def get_trading_pairs_for_assets(self, assets):
+        """
+        Retrieves trading pairs available for the specified assets.
+
+        Args:
+            assets (list): A list of asset names to check for trading pairs.
+
+        Returns:
+            list: A list of trading pair symbols that are currently trading.
+        """
         exchange_info = self.client.get_exchange_info()
         cleaned_assets = set(clean_asset_name(asset.upper()) for asset in assets)
         
@@ -27,6 +43,15 @@ class BinanceClient:
         ]
 
     def get_trade_history(self, symbol):
+        """
+        Fetches the trade history for a specific trading symbol.
+
+        Args:
+            symbol (str): The trading symbol to fetch the trade history for.
+
+        Returns:
+            list: A list of trades for the specified symbol, or an empty list if an error occurs.
+        """
         try:
             trades = self.client.get_my_trades(symbol=symbol)
             # Pretty print the JSON response
