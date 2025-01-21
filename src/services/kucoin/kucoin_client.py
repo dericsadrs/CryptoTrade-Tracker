71fbf3e768c5aefdd3e904ecc7ebc3d670b7f2a8
drc <drccsadrs@gmail.com>
2025-01-22 00:24:57 +0800

    feature: add kucoin class starter template

---
+++ src/services/kucoin/kucoin_client.py
@@ -0,0 +1,6 @@
+class KucoinClient:
+    def authenticate(self, api_key: str, api_secret: str, api_pass: str):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.api_pass
+    
